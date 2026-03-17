2ccbc6b4d0fa3b99e8151da8e6058d329cc4bb1e
Evgenii Kondratiev <9219210217@yandex.ru>
2026-03-17 20:54:39 +0300

    tasks update bug

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", views.tasks, name='tasks'),
     path("create_task/", views.create_task, name="create_task"),
     path("task/<int:id>", views.task_detail, name="task_detail"),
-    path("<int:id>/delete", views.task_update, name="task_update"),
+    path("<int:id>/update", views.task_update, name="task_update"),
     path("<int:id>/delete", views.task_delete, name="task_delete"),
 ]
