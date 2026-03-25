7b02d1a7364d981001f0a099878a5e0c0b27d328
Evgenii Kondratiev <9219210217@yandex.ru>
2026-03-25 20:16:40 +0300

    label delete bug fix

--- labels/views.py
+++ labels/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from labels.models import Label
 from .forms import LabelForm
 from django.contrib import messages
@@ -41,9 +42,11 @@ class DeleteLabelView(LoginRequiredMixin, DeleteView):
     template_name = 'labels/label_delete.html'
     success_url = reverse_lazy('labels:label_list')
 
-    def valid_form(self, form):
+    def form_valid(self, form):
         self.object = self.get_object()
         if self.object.tasks.exists():
             messages.error(self.request, 'Невозможно удалить метку')
+            return redirect('labels:label_list')
         else:
             messages.success(self.request, 'Метка успешно удалена')
+            return super().form_valid(form)
