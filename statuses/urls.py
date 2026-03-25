c8ff0ee4d2e60ce391dab53b5c20b67b63854f18
Evgenii Kondratiev <9219210217@yandex.ru>
2026-03-25 17:58:12 +0300

    final status crud change

--- statuses/urls.py
+++ statuses/urls.py
@@ -5,7 +5,13 @@ app_name = 'statuses'
 
 urlpatterns = [
     path("", views.StatusListView.as_view(), name='status_list'),
-    path("create/", views.StatusCreateView.as_view(), name='status_create'),
-    path("<int:pk>/update/", views.StatusUpdateView.as_view(), name='status_update'),
-    path("<int:pk>/delete/", views.StatusDeleteView.as_view(), name='status_delete')
+    path("create/", 
+         views.StatusCreateView.as_view(), 
+         name='status_create'),
+    path("<int:pk>/update/", 
+         views.StatusUpdateView.as_view(), 
+         name='status_update'),
+    path("<int:pk>/delete/", 
+         views.StatusDeleteView.as_view(), 
+         name='status_delete')
 ]
