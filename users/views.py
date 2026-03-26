d8d871f2bcc982b8f8fa8fd2ab69e3e983e65108
Evgenii Kondratiev <9219210217@yandex.ru>
2026-03-26 14:30:37 +0300

    user delete flash added

--- users/views.py
+++ users/views.py
@@ -77,22 +77,23 @@ class UserDeleteView(LoginRequiredMixin, DeleteView):
     def get_object(self, queryset=None):
         return get_object_or_404(User, pk=self.kwargs['pk'])
     
-    def delete(self, request, *args, **kwargs):
+    def form_valid(self, form):
         self.object = self.get_object()
         
-        if request.user.id != self.object.id:
-            messages.error(request, 'У вас нет прав для удаления этого пользователя')
+        if self.request.user.id != self.object.id:
+            messages.error(self.request, 'У вас нет прав для удаления этого пользователя')
             return redirect('users:users')
         
         if self.object.is_superuser:
             admin_count = User.objects.filter(is_superuser=True).count()
             if admin_count == 1:
-                messages.error(request, 'Нельзя удалить последнего администратора')
+                messages.error(self.request, 'Нельзя удалить последнего администратора')
                 return redirect('users:users')
         
-        username = self.object.username
-        messages.success(request, f'Пользователь "{username}" успешно удален')
-        return super().delete(request, *args, **kwargs)
+        response = super().form_valid(form)        
+        messages.success(self.request, 'Пользователь успешно удален')
+        
+        return response
 
 
 class CustomLogoutView(LogoutView):    
